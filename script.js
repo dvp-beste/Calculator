e11e04204756c9ac15a231b5a6e4eb1ef0213b13
Beste <besteeris@gmail.com>
2022-08-03 18:25:08 +0300

    Add decimal and change upperDisplay value
    Add decimal to the numbers. In order to be able to add the point the number1 and number2 elements are made string and converted to a number with parseFloat when displayed or used as argument in operate function.
    
    Disable decimal button if the numbers include a decimal and enable them again when entering a new number
    
    upperDisplay value should be different when the operator is = sign. For that case, add a conditional before calling the operate function. Call operate function only if the operator is different than = sign.

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function add(a, b) {
-	return Number(a) + Number(b);
+	return a + b;
 };
 
 function subtract(a, b) {
@@ -34,51 +34,62 @@ const operators = document.querySelectorAll('.operator');
 const display = document.getElementById('display');
 const upperDisplay = document.getElementById('upper-display');
 const clearer = document.getElementById('C');
-let storer = {number1:'' , number2:'', operator:''};
+const decimal = document.getElementById('.');
+let storer = {number1: '0' , number2: '0', operator:''};
+display.value = '0';
 
 clearer.addEventListener('click', () => {
-    storer = {number1:'' , number2:'', operator:''};
-    display.value = '';
+    storer = {number1: '0' , number2: '0', operator:''};
+    display.value = '0';
     upperDisplay.value = '';
+    decimal.disabled = false;
 })
 
 digits.forEach(item => {
     item.addEventListener('click', (e) => {
         if(storer.operator === '=') {
-            storer = {number1:'' , number2:'', operator:''};
+            storer = {number1: '0' , number2: '0', operator:''};
         }
         if(!storer.operator) {
             storer.number1 += e.target.id;
-            display.value = Number(storer.number1);
+            display.value = parseFloat(storer.number1);
         } else {
             storer.number2 += e.target.id;
-            display.value = Number(storer.number2);
+            display.value = parseFloat(storer.number2);
         }
-        
     })
 })
+
 
 
 operators.forEach(item => {
     item.addEventListener('click', (e) => {
+        decimal.disabled = false;
         if(Object.values(storer).every(item => item)){
-
-            upperDisplay.value = storer.number1 + storer.operator + storer.number2;
-            storer.number1 = operate(storer.operator, storer.number1, storer.number2);
-            storer.operator = e.target.id;
-            storer.number2 = '';
-            if(storer.operator !== '='){
-                upperDisplay.value = storer.number1 + storer.operator;
-                display.value = '';
-            } else {
-                display.value = '=' + storer.number1;
+            if(storer.operator === '='){
+                upperDisplay.value = parseFloat(storer.number1) + ' '+e.target.id;
+            } else{
+                upperDisplay.value = parseFloat(storer.number1) + ' ' + storer.operator +' '+ parseFloat(storer.number2) + ' ' + '=';
+                storer.number1 = operate(storer.operator, parseFloat(storer.number1), parseFloat(storer.number2));
             }
+            storer.operator = e.target.id;
+            storer.number2 = '0';
+            if(storer.operator !== '='){
+                upperDisplay.value = parseFloat(storer.number1) +' '+ storer.operator;
+            }
+            display.value = parseFloat(storer.number1);
         } else if(storer.number1){
             storer.operator = e.target.id;
-            upperDisplay.value = storer.number1 + storer.operator;
-            display.value = '';
+            upperDisplay.value = display.value +' '+ storer.operator;
+            if(storer.operator === '=') {
+                display.value = parseFloat(storer.number1);
+            } else {
+                display.value = '0';
+            }
+            
         }
     })
 })
+
 
 
